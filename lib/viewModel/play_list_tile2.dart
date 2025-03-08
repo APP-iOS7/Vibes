5952a42258641adbc776cb166f5482988a63ff11
김용해 <yoho1019@gmail.com>
2025-03-08 19:47:12 +0900

    데이터 인젝션 분리 이슈로 따로 생성 ( 버젼 v2)

---
+++ lib/viewModel/play_list_tile2.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:kls_project/model/VideoModel.dart';
+
+class PlayListTile2 extends StatelessWidget {
+  final VideoModel video;
+  const PlayListTile2({required this.video, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(video.title!,
+          style:
+              Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 16)),
+      subtitle: Row(
+        children: [
+          Text(video.uploadDate!),
+          SizedBox(width: 5.0),
+          Text(video.views!),
+        ],
+      ),
+      leading: Stack(
+        children: [
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Image.network(video.thumbnails!.first.url!),
+          ),
+          Positioned(
+            bottom: 3,
+            right: 3,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 1),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200.withValues(alpha: 0.1),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Text(
+                video.duration.toString().padLeft(2, "00"),
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

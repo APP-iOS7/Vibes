2ae7f6a0d20b1e0044e73278f0ac76ae9163d37a
김용해 <yoho1019@gmail.com>
2025-03-11 21:48:02 +0900

    DopeScreen 로직 및 UI 수정

--- lib/screen/dopeScreen.dart
+++ lib/screen/dopeScreen.dart
@@ -1,97 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-// 튜토리얼 오버레이 화면을 보여주는 위젯
-class DopeScreen extends StatelessWidget {
-  final VoidCallback nextMainPage; // nextMainPage 콜백 추가
-
-  const DopeScreen({Key? key, required this.nextMainPage}) : super(key: key);
+class DopeScreen extends StatefulWidget {
+  final VoidCallback nextMainPage;
+  const DopeScreen({required this.nextMainPage, super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: nextMainPage, // 콜백 실행
-          child: Text("튜토리얼 완료"),
-        ),
-      ),
-    );
-  }
+  State<DopeScreen> createState() => _DopeScreenState();
 }
 
-class TutorialOverlayScreen extends StatelessWidget {
+class _DopeScreenState extends State<DopeScreen> {
+  bool isScrollEnd = false;
+  final PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF606060),
       body: Stack(
         children: [
-          // 배경을 반투명한 검은색으로 설정하여 오버레이 효과 적용
-          Positioned.fill(
-            child: Opacity(
-              opacity: 0.7,
-              child: Container(color: Colors.black),
-            ),
+          PageView(
+            controller: _pageController,
+            onPageChanged: (int value) {
+              setState(() {
+                isScrollEnd = (value == 2);
+              });
+            },
+            children: [
+              _customPageView(intro: "검색은 총 20개의 데이터만 가져옵니다.", index: 1),
+              _customPageView(intro: "다운로드 한 음원은 앱 삭제시 꼭 지워주세요", index: 2),
+              _customPageView(intro: "마지막으로 다크모드는 시스템의 영향을 받습니다", index: 3),
+            ],
           ),
-          // 화살표와 설명이 포함된 박스를 특정 위치에 배치
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.55,
-            left: MediaQuery.of(context).size.width * 0.2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+          Container(
+            alignment: const Alignment(0, 0.85),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // 화살표를 그림으로 표시
-                CustomPaint(
-                  size: Size(100, 50),
-                  painter: ArrowPainter(),
+                GestureDetector(
+                  onTap: () => _pageController.jumpToPage(2),
+                  child:
+                      const Text("건너뛰기", style: TextStyle(color: Colors.white)),
                 ),
-                // 설명 텍스트가 포함된 박스
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    "이 버튼을 눌러 음악을 검색하세요!",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                SmoothPageIndicator(
+                  controller: _pageController,
+                  count: 3,
+                  effect: ExpandingDotsEffect(
+                    dotColor: Colors.white.withValues(alpha: 0.5),
+                    activeDotColor: Colors.white,
                   ),
                 ),
+                isScrollEnd
+                    ? GestureDetector(
+                        onTap: widget.nextMainPage,
+                        child: const Text("이동",
+                            style: TextStyle(color: Colors.white)),
+                      )
+                    : GestureDetector(
+                        onTap: () => _pageController.nextPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeOut,
+                        ),
+                        child: const Text("다음",
+                            style: TextStyle(color: Colors.white)),
+                      ),
               ],
             ),
-          ),
-          // 튜토리얼 종료 버튼
-          Positioned(
-            bottom: 50,
-            left: MediaQuery.of(context).size.width * 0.25,
-            child: ElevatedButton(
-              onPressed: () => Navigator.pop(context), // 버튼 클릭 시 화면 닫기
-              child: Text("튜토리얼 종료"),
-            ),
-          ),
+          )
         ],
       ),
     );
   }
-}
 
-// 화살표를 그리는 커스텀 페인터 클래스
-class ArrowPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.white // 화살표 색상 설정
-      ..strokeWidth = 5 // 선 두께 설정
-      ..style = PaintingStyle.stroke; // 선 스타일 설정
-
-    final path = Path()
-      ..moveTo(size.width, size.height) // 시작점 설정
-      ..lineTo(size.width / 2, 0) // 위쪽 끝점으로 선을 그림
-      ..lineTo(0, size.height); // 왼쪽 끝점으로 선을 그림
-
-    canvas.drawPath(path, paint); // 캔버스에 화살표 그리기
+  // 첫번째 스크롤 페이지
+  Column _customPageView({required int index, required String intro}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(intro,
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Colors.white)),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 50, horizontal: 70),
+          child: Image.asset("assets/images/dope$index.jpeg"),
+        ),
+      ],
+    );
   }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
-      false; // 화살표는 변하지 않으므로 false 반환
 }
